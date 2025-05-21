bb1a448fa4ac14f168f21b2071d1490db68433dd
TelieP <151426758+TelieP@users.noreply.github.com>
2025-05-21 13:04:06 +0200

    Delete EcoRide directory

--- EcoRide/test/confirmation_ajout_trajet.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include('../includes/connect.php');
-include('../includes/header.php');
-
-echo "votre le trajet a bien été ajouté !" . "<br>";
-?>
-<br><br><br>
-<strong><a href="../profil.php">Retour au profil</a></strong>
